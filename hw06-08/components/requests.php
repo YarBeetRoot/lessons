c069b4eff882eb95edc3ae3cf80e8137e0b63ce1
YaroslavL <lizogub.yar@gmail.com>
2018-08-08 14:46:24 +0300

    update v.1.0.1

--- hw06-08/components/requests.php
+++ hw06-08/components/requests.php
@@ -2,25 +2,30 @@
 include 'components/db.php';
 include 'components/validate.php';
 
-function getStudentsData() {
+function makeRequest($sql, $options = []){
     $dbConnection = getConnection();
-    $sth = $dbConnection->prepare('SELECT student.name, `age`, university.name as universityName, university.description as universityDescr FROM `student`
+    $sth = $dbConnection->prepare($sql);
+    $sth->execute($options);
+    return $sth;
+}
+
+function getStudentsData() {
+    $sql = 'SELECT student.name, `age`, university.name as universityName, university.description as universityDescr FROM `student`
 INNER JOIN university 
-ON student.university_id = university.id');
-    $sth->execute();
+ON student.university_id = university.id';
+    $sth = makeRequest($sql);
     return $sth->fetchAll();
 }
 function insertStudent() {
-    $dbConnection = getConnection();
-    $sth = $dbConnection->prepare('INSERT INTO student
-    (name, age, university_id)
-     VALUES (:name, :age, :university_id)');
 
-    return $sth->execute([
-            'name' => $_POST['name'],
-            'age' => $_POST['age'],
-            'university_id' => $_POST['universityId'],
-        ]
-    );
+    $sql = 'INSERT INTO student
+    (name, age, university_id)
+     VALUES (:name, :age, :university_id)';
+    $sth = makeRequest($sql, [
+        'name' => $_POST['name'],
+        'age' => $_POST['age'],
+        'university_id' => $_POST['universityId'],
+    ]);
+    return $sth;
 }
 
